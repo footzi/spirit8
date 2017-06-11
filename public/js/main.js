5a302af74d66a8c00a40fce07b4a91ab873a3fc6
footzi <footzi@mail.ru>
2017-06-11 12:15:42 +0300

    no message

--- public/js/main.js
+++ public/js/main.js
@@ -25,22 +25,72 @@ $(document).ready(function() {
                 $(".navbar").removeClass("navbar-fixed")
             }
         })
-    })()
-});
+    })();
+
+    (function smoothscroll() {
+        $("a").on('click', function(event) {
+        if (this.hash !== "") {
+          event.preventDefault();
+          var hash = this.hash;
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+            }, 800, function(){
+              window.location.hash = hash;
+          });
+        } 
+        });    
+    })();
+
+    
 
 
 
+    //(function navbarlink () {
+      //   $(".navbar a:eq(1)").addClass("active");
+      //   $(window).scroll(function() {
+      //       var scroll=$(this).scrollTop();
+      //       var header=$("header").height();
+      //       var services=header+$(".services").height()+$(".team").height()
 
-$(document).ready(function(){
-  $("a").on('click', function(event) {
-    if (this.hash !== "") {
-      event.preventDefault();
-      var hash = this.hash;
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-        window.location.hash = hash;
-      });
-    } 
+            
+
+      //       if (scroll>header) {
+      //           $(".navbar a:eq(1)").removeClass("active")
+      //           $(".navbar a:eq(2)").addClass("active")
+      //       } else {
+      //         $(".navbar a:eq(2)").removeClass("active")
+      //       }
+
+      // })
+
+    //   $(window).scroll(function() {
+    //     var scroll=$(window).scrollTop();
+    //     $("body").each(function(i) {
+    //         if($(this))
+
+    //     })
+    //   })
+    // })()
+    $('.navbar a').on('click', function() {
+      var scrollAnchor = $(this).attr('data-scroll'),
+          scrollPoint = $('section[data-anchor="' + scrollAnchor + '"]').offset().top;
+      $('body,html').animate({
+          scrollTop: scrollPoint
+      }, 500);
+      return false;
   });
+
+
+
+
+    $(window).scroll(function() {
+  var windscroll = $(window).scrollTop();
+    $('section').each(function(i){
+      if($(this).position().top <= windscroll + 10) {
+        $('.navbar a.active').removeClass('active');
+        $('.navbar a').eq(i).addClass('active');
+      }
+    });
+  }).scroll();
 });
+
